7e7bc973d01363b4c75ed35eb6f8e97c611e1ae1
songyi <songyee112@naver.com>
2023-09-12 17:55:32 +0900

    뉴스 메인페이지 실행 가능한 상태로 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Categories from './components/News/Categories';
+import Categories from './components/NewsPage/Categories';
 import MainNews from './components/Main/MainNews';
 import MainNewsItem from './components/Main/MainNewsItem';
-import NewsList from './components/News/NewsList';
+import NewsList from './components/NewsPage/NewsList';
 import usePromise from './components/hook/usePromise';
-
-
-
+import { useCallback, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
