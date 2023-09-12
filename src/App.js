559fd228aeb7fa5f2da3a459afc847144da0356c
songyeee <songyee112@naver.com>
2023-09-12 15:17:50 +0900

    뉴스 만들 계획

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from './components/News/Categories';
+import MainNews from './components/Main/MainNews';
+import MainNewsItem from './components/Main/MainNewsItem';
+import NewsList from './components/News/NewsList';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainNews />
     </div>
   );
 }
---
+++ src/components/Main/MainNews.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import MainNewsItem from './MainNewsItem';
+
+
+
+function MainNews() {
+  return (
+    <div>MainNews</div>
+  )
+}
+console.log('하이하이')
+
+export default MainNews;
