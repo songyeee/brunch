05e7dafb5d3c4b50a33a0aac39aab4ee20d5a0c1
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-09-14 11:08:36 +0900

    회색 큰 테두리 만들기

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Categories from './components/News/Categories';
 import MainNews from './components/Main/MainNews';
 import MainNewsItem from './components/Main/MainNewsItem';
 import NewsList from './components/News/NewsList';
-
-
+import Weather1 from './Weather/Weather1';
+import styled from 'styled-components';
 
 
 function App() {
   return (
     <div className="App">
-      <MainNews />
+      {/* <MainNews />  */}
+      <Weather1 />
     </div>
   );
 }
--- src/Weather/Weather1.jsx
+++ src/Weather/Weather1.jsx
@@ -3,6 +3,22 @@ import { async } from 'q';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const ContainerWrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    border: 1px gray solid;
+
+    .Weather1{
+        left: 50%;
+        top:50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+        border: 1px blue solid;
+        padding: 20px;
+    }
+`;
+
+
 function Weather1() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=Busan&appid=13370407832c91a4e9588f1ce73f66111`;
     const [loacation, setLocation] = useState('');
@@ -20,15 +36,10 @@ function Weather1() {
     }
 
   return (
-    <containerWrap>
-        <div className='weahterbox1'>
-
-        </div>
-    </containerWrap>
+    <ContainerWrap>
+        <div className='weahterbox1'></div>
+    </ContainerWrap>
   )
 }
 
 export default Weather1;
-
-const weahterbox1 = styled.div
-width:100vw;
