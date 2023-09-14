b065f4176729b3fed4c220de153282312487d594
songyi <songyee112@naver.com>
2023-09-14 12:48:52 +0900

    뉴스페이지 css 테두리,  글자크기, 정렬 수정완료

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import MainNewsItem from './components/Main/MainNewsItem';
 import usePromise from './components/hook/usePromise';
 import { useCallback, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+      <div style={{width:'50%'}}>abcd</div>
       <MainNews />
+      </div>
     </div>
   );
 }
--- src/components/Main/MainNewsItem.jsx
+++ src/components/Main/MainNewsItem.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const MainNewsItemBlock = styled.div``;
+const MainNewsItemBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  margin: 1rem;
+  box-shadow: 0px 0px 8px 0px rgba(199, 199, 199, 0.68);
+  border-radius: 10px;
+
+
+  a{
+    display: flex;
+    color: black;
+    text-decoration: none;
+    font-size: 10pt;
+  }
+
+  img{
+    display: block;
+    width: 160px;
+    height: 100px;
+    object-fit: cover;
+    margin-right: 2rem;
+    border-radius: 10px;
+  }
+
+  h1 {
+    font-size: 12pt;
+    }
+
+  article {
+    align-content:center;
+  }
+  `
+ 
 
 function MainNewsItem({article}) {
   const { title, url, urlToImage, description } = article;
@@ -9,8 +41,10 @@ function MainNewsItem({article}) {
     <MainNewsItemBlock>
       <a href={url}>
         <img src = {urlToImage ?? 'https://via.placeholder.com/160'} alt='이미지 없음' />
+        <div className='article'>
         <h1>{title}</h1>
         <p>{description}</p>
+        </div>
       </a>
     </MainNewsItemBlock>
     )
