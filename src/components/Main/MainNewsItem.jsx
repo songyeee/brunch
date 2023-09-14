ae022c6f4a1f557761684187b2637365f469c80f
songyi <songyee112@naver.com>
2023-09-14 11:32:48 +0900

    헤더 로고삽입, 중간 정렬

--- src/components/Main/MainNewsItem.jsx
+++ src/components/Main/MainNewsItem.jsx
@@ -4,12 +4,13 @@ import styled from 'styled-components';
 const MainNewsItemBlock = styled.div``;
 
 function MainNewsItem({article}) {
-  const { title, url, urlToImage } = article;
+  const { title, url, urlToImage, description } = article;
   return (
     <MainNewsItemBlock>
       <a href={url}>
         <img src = {urlToImage ?? 'https://via.placeholder.com/160'} alt='이미지 없음' />
         <h1>{title}</h1>
+        <p>{description}</p>
       </a>
     </MainNewsItemBlock>
     )
